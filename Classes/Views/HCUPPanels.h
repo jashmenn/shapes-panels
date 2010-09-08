6b195778077f927d4a38ab74e98979c217ca9166
Nate Murray <nate@natemurray.com>
2010-09-07 17:57:37 -0700

    Added hccup panels

---
+++ Classes/Views/HCUPPanels.h
@@ -0,0 +1,15 @@
+/*
+ *  HCUPPanels.h
+ *  shapes-panels
+ *
+ *  Created by Nate Murray on 9/7/10.
+ *  Copyright 2010 LittleHiccup. All rights reserved.
+ *
+ */
+
+#import "cocos2d.h"
+
+@interface HCUPPanels : CCLayer {
+}
+
+@end
